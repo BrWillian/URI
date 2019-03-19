00af3bf95373d105ff7589e59f6214e993957a21
Willian.Antunes <wiliam-m-@hotmail.com>
2019-03-19 09:19:47 -0300

    Exercicios Resolvidos Uri

---
+++ 2752.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+int main()
+{
+	ll a,b;
+
+	scanf("%lld.%lld",&a,&b);
+	printf("%lld.%lld\n",b,a);
+
+}
---
+++ 2753.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for(int i=97; i<=122; i++)
+    {
+        printf("%d e %c\n",i,i);
+    }
+
+}
